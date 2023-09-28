fbd8beb86af2262183f9f7961a100c13458f12b9
Rajanan <g.rajan1993@gmail.com>
2023-09-28 13:08:00 +0000

    fix(config): fix hmr in devcontainer

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
-};
-
+	plugins: [sveltekit()],
+	server: {
+		port: 5173,
+		hmr: {
+			port: 5173,
+		},
+		watch: {
+			usePolling: true
+		},
+	}
+}
 export default config;
